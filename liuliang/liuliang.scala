de5eb1f65379930b5631f6dea72556e4f8ef2d1c
Hypnoes <dota2cn@163.com>
2017-04-02 19:55:34 +0800

    1.3

--- liuliang/liuliang.scala
+++ liuliang/liuliang.scala
@@ -8,25 +8,32 @@ object liuliang {
     // The Main Entery.
     def main(args: Array[String]): Unit = {
 
-        val r1 = """((\d{1,3}\.){3}\d{1,3}\.?[\w\-]{0,}(\s>\s)?){2}""".r // Find all IP and counting
-        val r2 = """(\d{1,3}\.){3}\d{1,3}\.?[\w\-]{0,}(\s>\s)""".r       // Find Only Sender
-        val r3 = """(\s>\s)(\d{1,3}\.){3}\d{1,3}\.?[\w\-]{0,}""".r       // Find Only Receiver
+        val r1 = """((\d{1,3}\.){3}\d{1,3}\.?[\w\-]{0,}(\s>\s)?){2}""".r 
+        val r2 = """(\d{1,3}\.){3}\d{1,3}\.?[\w\-]{0,}(\s>\s)""".r       
+        val r3 = """(\s>\s)(\d{1,3}\.){3}\d{1,3}\.?[\w\-]{0,}""".r       
 
         val spark = new SparkContext("spark://10.170.31.120:7077", "liuliang")
         val fs = spark.textFile("hdfs://10.170.31.120:9000/user/hypnoes/liuliang.txt")
-        val meth = default(fs)(_, _)
+        val meth1 = default(fs)(_,_)
+        val meth2 = direct(fs)(_)
         args(0) match {
-            case "-o" => meth(r3, 3)
-            case "-i" => meth(r2, 2)
-            case "-d" => meth(r1, 0)
-            case _    => meth(r1, 1)
+            case "-o" => meth1(r3, "out")        // Find Only Receiver
+            case "-i" => meth1(r2, "in")         // Find Only Sender
+            case "-d" => meth2(r1)               // Find the direction
+            case _    => meth1(r1, "all")        // Find all IP and counting
             }
         spark.stop()
     }
 
     // Default Method.
-    def default(fs: RDD[String])(r: scala.util.matching.Regex, mod: Int = 0): Unit = {
-        fs.flatMap(line => r.findAllIn(line)).flatMap(line => if(mod > 0) line else line.split(" > ")).map(word =>
-            (word, 1)).reduceByKey(_+_).sortBy(x => x._2, false).saveAsTextFile("hdfs://10.170.31.120:9000/user/hypnoes/ans")
+    def default(fs: RDD[String])(r: scala.util.matching.Regex, name: String): Unit = {
+        fs.flatMap(line => r.findAllIn(line)).flatMap(line => line.split(" > ")).map(word =>
+            (word, 1)).reduceByKey(_+_).sortBy(x => x._2, false).saveAsTextFile("hdfs://10.170.31.120:9000/user/hypnoes/ans-" + name)
+    }
+
+    // Directed Method.
+    def direct(fs: RDD[String])(r: scala.util.matching.Regex): Unit = {
+        fs.flatMap(line => r.findAllIn(line)).map(word => 
+        (word, 1)).reduceByKey(_+_).sortBy(x => x._2, false).saveAsTextFile("hdfs://10.170.31.120:9000/user/hypnoes/ans-default")
     }
 }
