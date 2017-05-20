1d8110be534d29d9a1eb38bdbaacfb0a2afa9e98
Hypnoes <dota2cn@163.com>
2017-05-21 02:02:31 +0800

    Update CusModelClus DF.ver

--- Powering_ChengDu/CusModelClustering/DataFrame/build.sbt
+++ Powering_ChengDu/CusModelClustering/DataFrame/build.sbt
@@ -1,7 +1,7 @@
 lazy val root = (project in file(".")).
     settings(
         name := "PoweringChengDu",
-        version := "1.2",
+        version := "1.3",
         scalaVersion := "2.11.8",
         libraryDependencies ++= Seq(
             "org.apache.spark" % "spark-core_2.11" % "2.1.0",
--- Powering_ChengDu/CusModelClustering/DataFrame/ext.scala
+++ Powering_ChengDu/CusModelClustering/DataFrame/ext.scala
@@ -1,27 +1,42 @@
 import org.apache.spark.sql.SparkSession
-import org.apache.spark.ml.clustering.KMeans
+import org.apache.spark.sql.{Dataset, DataFrame}
+import org.apache.spark.ml.clustering.{KMeans, KMeansModel}
 import org.apache.spark.ml.linalg.Vectors
 
 object PoweringChengDu {
-  def main(args: Array[String]): Unit = {
-    val spark = SparkSession
-        .builder
-        .appName(s"${this.getClass.getSimpleName}")
-        .getOrCreate()
+    def main(args: Array[String]): Unit = {
+        val spark = SparkSession
+            .builder
+            .appName(s"${this.getClass.getSimpleName}")
+            .getOrCreate()
 
-    val root = "hdfs://10.170.31.120:9000/user/hypnoes/dsq_1M_svm.txt"
-    val dataset = spark.read.format("libsvm").load(root)
+        val root = "hdfs://10.170.31.120:9000/user/hypnoes/"
+        args.foreach(input => {
+            train(spark.read.format("libsvm").load(root + "SVM_Data/" +input),
+                input)
+        })
 
-    val kmm = new KMeans().setK(3)
-    val model = kmm.fit(dataset)
+        spark.stop()
+    }
 
-    val WSSSE = model.computeCost(dataset)
-    println(s"Within Set Sum of Squared Errors = $WSSSE")
+    def train(df: DataFrame, name: String): Unit = {
+        val kmm = new KMeans().setK(3)
+        val model = kmm.fit(df)
 
-    println("Cluster Centers: ")
-    model.clusterCenters.foreach(println)
+        writeOut(model, name)
 
-    spark.stop()
-  }
+        model.transform(df).write
+            .json("hdfs://10.170.31.120:9000/user/hypnoes/"
+                + "out/" + name.split("svm")(0) + "a")
+    }
+
+    def writeOut(model: KMeansModel, name: String): Unit = {
+        println("")
+        println("=======================" + name + "===========================")
+        println("Cluster Centers: ")
+        model.clusterCenters.foreach(println)
+        println("=======================" + name + "===========================")
+        println("")
+    }
 }
 
