583c5d75f8d6870cdd34337cea86212b2512ddf0
Hypnoes <dota2cn@163.com>
2017-05-26 20:36:33 +0800

    Merge to New API

---
+++ Powering_ChengDu/liuliang/DataFrame/build.sbt
@@ -0,0 +1,10 @@
+lazy val root = (project in file(".")).
+    settings(
+        name := "liuliang",
+        version := "2.0",
+        scalaVersion := "2.11.8",
+        libraryDependencies ++= Seq(
+            "org.apache.spark" % "spark-core_2.11" % "2.1.0",
+            "org.apache.spark" % "spark-sql_2.11" % "2.1.0"
+        )
+    ) 
---
+++ Powering_ChengDu/liuliang/DataFrame/liuliang.scala
@@ -0,0 +1,23 @@
+import org.apache.spark.sql
+import org.apache.spark.sql._
+
+object Liuliang {
+    def main(args: Array[String]): Unit = {
+        val spark= SparkSession
+            .builder
+            .appName(s"${this.getClass.getSimpleName}")
+            .getOrCreate()
+        
+        val root = "hdfs://10.170.31.120:9000/user/hypnoes"
+        val r = """((\d{1,3}\.){3}\d{1,3}\.?[\w\-]{0,}(\s>\s)?){2}""".r 
+        
+        val raw = spark.read.text(root + "liuliang.txt").as[String]
+        val df = raw.map(r.findAllIn(_)).map(_.split(" > ")).map(x => (x(0), x(1)))
+            .toDF("IN", "OUT").groupBy("IN", "OUT").count.sort($"Count".desc)
+
+        df.write.json(root + "blas")
+
+        spark.stop()
+
+    }
+}
