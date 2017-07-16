4582177bd73b5d6f1d51569a49b3ced337bd012a
Hypnoes <dota2cn@163.com>
2017-07-16 23:32:48 +0800

    OneByOne

--- Powering_ChengDu/CusFlowStAnly/CusFlowStA.scala
+++ Powering_ChengDu/CusFlowStAnly/CusFlowStA.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.linalg.Vectors
 
-object PoweringChengDu {
+object CusFlowStA {
     def main(args: Array[String]): Unit = {
         // TO DO HERE ...
     }
--- Powering_ChengDu/CusModelClustering/DataFrame/CusMoC.scala
+++ Powering_ChengDu/CusModelClustering/DataFrame/CusMoC.scala
@@ -2,7 +2,7 @@ import org.apache.spark.sql.SparkSession
 import org.apache.spark.ml.clustering.KMeans
 import org.apache.spark.ml.linalg.Vectors
 
-object PoweringChengDu {
+object CusModelCluster {
     def main(args: Array[String]): Unit = {
         val spark = SparkSession
             .builder
